5f7b88c2c51fdece834664775b4b173eaf4b01df
Buchenkov <igorchan@mail.ru>
2024-05-08 21:02:06 +0300

    start

---
+++ bulletin_boards/bulletin_boards/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for bulletin_boards project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from .views import ArticleList, ArticleDetail, ArticleCreate, ArticleUpdate, ArticleDelete, CommentCreate
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', ArticleList.as_view()),
+    path('article/<int:pk>/', ArticleDetail.as_view(), name='post'),
+    path('article/articles_create/', ArticleCreate.as_view(), name='articles_create'),
+    path('articles/<int:pk>/edit', ArticleUpdate.as_view(), name='articles_edit'),
+    path('article/<int:pk>/delete', ArticleDelete.as_view(), name='article_delete'),
+    path('<int:pk>/comment/create/', CommentCreate.as_view(), name='comment_create'),
+    path('accounts/', include('allauth.urls')),
+]
