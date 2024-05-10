22c7f8c73bc4405880371255ef2923063e2d9191
Buchenkov <igorchan@mail.ru>
2024-05-10 19:58:17 +0300

    код -

--- bulletin_boards/bulletin_boards/settings.py
+++ bulletin_boards/bulletin_boards/settings.py
@@ -165,7 +165,6 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'  # 'none'
 # # mandatory — не пускать пользователя на сайт до момента подтверждения почты;
 # # optional — сообщение о подтверждении почты будет отправлено, но пользователь может залогиниться на сайте без подтверждения почты.
-ACCOUNT_FORM = {'signup': 'accounts.forms.CommonSignupForm'}
 ACCOUNT_FORMS = {'signup': 'accounts.forms.CommonSignupForm'}
 # ACCOUNT_CONFIRM_EMAIL_ON_GET = True  # позволит избежать дополнительного входа и активирует аккаунт сразу,
 # # как только мы перейдём по ссылке.
